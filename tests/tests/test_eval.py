b0d9fab266b613bcbe7ea7d6d5cd8644bbbb4c50
Akash Gautam <418akash@gmail.com>
2023-04-21 16:03:23 +0200

    changing tests

---
+++ tests/tests/test_eval.py
@@ -0,0 +1,117 @@
+from unittest.mock import MagicMock
+from hf_scripts.utility_functions import *
+import os
+import pytest
+import unittest
+import process_args
+from transformers import HfArgumentParser, TrainingArguments
+from hf_scripts.data_trainining_args import DataTrainingArguments
+from tasks import *
+from hf_scripts.hgf_fine_tune_class import *
+from hf_scripts.hgf_fine_tune_ner import *
+from hf_scripts.hgf_fine_tune_qa import *
+import tempfile
+
+"""
+File consisting of integeration unit test cases for evaluating each of the tasks
+"""
+
+
+#@pytest.mark.skip()
+def test_cls_evaluation():
+    temp_dir_name = tempfile.TemporaryDirectory().name
+    data_args = DataTrainingArguments()
+    data_args.dataset_name = "akash418/germeval_2017"
+    #data_args.base_checkpoint_dir = "/tmp/directory"
+    data_args.base_checkpoint_dir = temp_dir_name
+    #print("temp directory ....", tempfile.TemporaryDirectory().name)
+    data_args.is_task_ner = False
+    data_args.label_value = "relevance"
+    data_args.peft_choice = "p_tune"
+
+    model_args = ModelArguments("bert-base-german-cased")
+    model_args.model_name_or_path = "bert-base-german-cased"
+    model_args.model_revision = "main"
+    model_args.use_fast_tokenizer = False
+    
+    training_args = TrainingArguments(output_dir = temp_dir_name)
+    #training_args = TrainingArguments(output_dir="/tmp/directory")
+    #training_args.output_dir = "/tmp/directory"
+    training_args.output_dir = temp_dir_name
+    training_args.num_train_epochs = 1
+    training_args.do_train = True
+    training_args.do_eval = True
+    training_args.do_predict = True
+    training_args.per_device_train_batch_size = 128
+
+    init_args = InitialArguments()
+    #init_args.results_logging_dir = "/tmp/directory"
+    init_args.results_logging_dir = temp_dir_name
+    init_args.task_list = "germeval2017"
+
+    metrics_eval = hgf_fine_tune_class.run_task_evaluation(model_args, data_args, training_args, init_args)
+    assert metrics_eval['eval_accuracy'] == pytest.approx(0.81, 0.3)
+
+
+@pytest.mark.skip()
+def test_ner_evaluation():
+    data_args = DataTrainingArguments()
+    data_args.dataset_name = "elenanereiss/german-ler"
+    data_args.base_checkpoint_dir = "/tmp/directory"
+    data_args.is_task_ner = True
+    # data_args.label_value="multi"
+    data_args.peft_choice = "prefix_tune"
+    data_args.return_entity_level_metrics = True
+
+    model_args = ModelArguments("bert-base-german-cased")
+    model_args.model_name_or_path = "bert-base-german-cased"
+    model_args.model_revision = "main"
+    model_args.use_fast_tokenizer = True
+
+    training_args = TrainingArguments(output_dir="/tmp/directory")
+    training_args.output_dir = "/tmp/directory"
+    training_args.num_train_epochs = 1
+    training_args.do_train = True
+    training_args.do_eval = True
+    #training_args.do_predict = True
+    training_args.per_device_train_batch_size=64
+
+    init_args = InitialArguments()
+    init_args.results_logging_dir = "/tmp/directory"
+    init_args.task_list = "german_ner"
+
+    metrics_eval = hgf_fine_tune_ner.run_task_evaluation(model_args, data_args, training_args, init_args)
+    assert metrics_eval['eval_overall_accuracy'] == pytest.approx(0.90, 0.3)
+
+
+@pytest.mark.skip()
+def test_qa_evaluation():
+    data_args = DataTrainingArguments()
+    data_args.dataset_name = "deepset/germanquad"
+    data_args.base_checkpoint_dir = "/tmp/directory"
+    data_args.is_task_ner = False
+    # data_args.label_value="multi"
+    data_args.peft_choice = "prompt_tune"
+    data_args.version_2_with_negative = True
+
+    model_args = ModelArguments("bert-base-german-cased")
+    model_args.model_name_or_path = "bert-base-german-cased"
+    model_args.model_revision = "main"
+    model_args.use_fast_tokenizer = True
+
+    training_args = TrainingArguments(output_dir="/tmp/directory")
+    training_args.output_dir = "/tmp/directory"
+    training_args.num_train_epochs = 1
+    training_args.do_train = True
+    training_args.do_eval = True
+    training_args.do_predict = True
+    training_args.overwrite_output_dir = True
+
+    init_args = InitialArguments()
+    init_args.results_logging_dir = "/tmp/directory"
+    init_args.task_list = "german_quad"
+
+
+    metrics_eval = hgf_fine_tune_qa.run_task_evaluation(model_args, data_args, training_args, init_args)
+    assert metrics_eval['eval_f1'] == pytest.approx(50.99, 0.3)
+
---
+++ tests/tests/test_utils.py
@@ -0,0 +1,201 @@
+from unittest.mock import MagicMock
+from hf_scripts.utility_functions import *
+import os
+import pytest
+import unittest
+from process_args import process_arguments
+from transformers import HfArgumentParser, TrainingArguments
+from hf_scripts.data_trainining_args import DataTrainingArguments
+from tasks import *
+import tasks
+from hf_scripts.hgf_fine_tune_class import *
+from hf_scripts.hgf_fine_tune_ner import *
+from hf_scripts.hgf_fine_tune_qa import *
+
+
+"""
+File consisting of integeration unit test cases for utility functions (implemented logic)
+"""
+
+@pytest.mark.skip()
+def test_add_labels_args():
+    data_args = DataTrainingArguments()
+    sample_task = "germeval2018"
+    TASK_REGISTRY = {"germeval2018": germeval2018.GermEval2018}
+    germeval_obj = germeval2018.GermEval2018()
+    germeval_obj.get_dataset_id = MagicMock(return_value="philschmid/germeval18")
+    germeval_obj.get_task_type = MagicMock(return_value="classification")
+    germeval_obj.get_label_name = MagicMock(return_value="label")
+    assert add_labels_data_args(sample_task, data_args) == data_args
+
+
+@pytest.mark.skip()
+def test_prepend_data_args():
+    data_args = DataTrainingArguments()
+    training_args = TrainingArguments(output_dir="/sample/directory")
+    init_args = InitialArguments()
+    init_args.results_logging_dir = "/sample/directory"
+    training_args.output_dir = "/sample/directory"
+    assert prepend_data_args(training_args, data_args, init_args) == (
+        training_args,
+        data_args,
+    )
+
+
+#@pytest.mark.skip()
+def test_freeze():
+    model_args = ModelArguments(model_name_or_path="bert-base-german-cased")
+    model_args.freeze_layers = True
+    model = AutoModelForTokenClassification.from_pretrained("bert-base-german-cased")
+    assert freeze_layers(model_args, model) == model
+
+
+@pytest.mark.skip()
+def test_load_config():
+    model_name_or_path = "bert-base-german-cased"
+    num_labels = 1
+    finetuning_task = "classification"
+    cache_dir = "/sample/directory"
+    model_revision = "main"
+    use_auth_token = False
+    model_type = "classification"
+
+    config = AutoConfig.from_pretrained(
+        model_name_or_path,
+        num_labels=num_labels,
+        fine_tuning_task=finetuning_task,
+        cache_dir=cache_dir,
+        revision=model_revision,
+        use_auth_token=use_auth_token,
+    )
+    assert (
+        load_config(
+            model_name_or_path,
+            num_labels,
+            finetuning_task,
+            cache_dir,
+            model_revision,
+            use_auth_token,
+            model_type,
+        )
+        == config
+    )
+
+
+@pytest.mark.skip()
+def test_load_model():
+    model_name_or_path = "bert-base-german-cased"
+    finetuning_task = "question-answering"
+    cache_dir = "/sample/directory"
+    model_revision = "main"
+    use_auth_token = False
+    model_type = "qa"
+    config = AutoConfig.from_pretrained(
+        model_name_or_path,
+        fine_tuning_task=finetuning_task,
+        cache_dir=cache_dir,
+        revision=model_revision,
+        use_auth_token=use_auth_token,
+    )
+    use_fast = False
+    model = AutoModelForQuestionAnswering.from_pretrained(
+        model_name_or_path,
+        from_tf=False,
+        # config=config,
+        cache_dir=cache_dir,
+        revision=model_revision,
+        use_auth_token=use_auth_token,
+    )
+
+    assert (
+        load_model(
+            model_name_or_path,
+            False,
+            config,
+            cache_dir,
+            model_revision,
+            False,
+            "qa",
+        ).base_model_prefix
+        == "bert"
+    )
+
+
+@pytest.mark.skip()
+def test_tasks_initialization():
+    test_task = "german_ner_legal"
+    assert isinstance(get_all_tasks(), list)
+
+
+@pytest.mark.skip()
+def test_process_args():
+    tasks_mock_obj = tasks
+    tasks_mock_obj.get_all_tasks = MagicMock(return_value = ["germeval2018"])
+
+    sample_cli_args = [
+        "--model_name_or_path",
+        "bert-base-german-cased",
+        "--task_list",
+        "germeval2018",
+        "--base_checkpoint_dir",
+        "/sample/directory",
+        "--results_logging_dir",
+        "/sample/directory",
+        "--peft_choice",
+        "lora",
+        "--output_dir",
+        "/sample/directory",
+    ]
+    assert isinstance(process_arguments(sample_cli_args), HfArgumentParser)
+
+
+@pytest.mark.skip()
+def test_get_labels():
+    label_list = ["binary", "multi"]
+    assert isinstance(get_label_list(label_list), list)
+
+
+@pytest.mark.skip()
+def test_map_source_file():
+    task_name = "gnad10"
+    assert type(map_source_file(task_name))
+
+
+
+@pytest.mark.skip()
+def test_init():
+    germeval_obj = germeval2017.GermEval2017()
+    assert isinstance(germeval_obj.get_url(), str)
+    assert isinstance(german_europarl.GermanEuroParl().get_task_name(), str)
+    assert isinstance(german_europarl.GermanEuroParl().get_task_type(), str)
+    assert isinstance(german_europarl.GermanEuroParl().get_dataset_id(), str)
+    assert isinstance(german_europarl.GermanEuroParl().get_url(), str)
+
+    assert isinstance(german_ner.GermanNerLegal().get_url(), str)
+    assert isinstance(german_ner.GermanNerLegal().get_task_type(), str)
+    assert isinstance(german_ner.GermanNerLegal().get_task_name(), str)
+    assert isinstance(german_ner.GermanNerLegal().get_dataset_id(), str)
+
+    assert isinstance(german_quad.GermanQuad().get_dataset_id(), str)
+    assert isinstance(german_quad.GermanQuad().get_task_type(), str)
+    assert isinstance(german_quad.GermanQuad().get_task_name(), str)
+    assert isinstance(german_quad.GermanQuad().get_url(), str)
+
+    assert isinstance(gnad10.Gnad10().get_dataset_id(), str)
+    assert isinstance(gnad10.Gnad10().get_task_type(), str)
+    assert isinstance(gnad10.Gnad10().get_url(), str)
+    assert isinstance(gnad10.Gnad10().get_task_name(), str)
+    assert isinstance(gnad10.Gnad10().get_label_name(), str)
+
+    assert isinstance(germeval2017.GermEval2017().get_dataset_id(), str)
+    assert isinstance(germeval2017.GermEval2017().get_task_type(), str)
+    assert isinstance(germeval2017.GermEval2017().get_url(), str)
+    assert isinstance(germeval2017.GermEval2017().get_task_name(), str)
+    assert isinstance(germeval2017.GermEval2017().get_label_name(), str)
+
+    assert isinstance(germeval2018.GermEval2018().get_dataset_id(), str)
+    assert isinstance(germeval2018.GermEval2018().get_task_type(), str)
+    assert isinstance(germeval2018.GermEval2018().get_url(), str)
+    assert isinstance(germeval2018.GermEval2018().get_task_name(), str)
+    assert isinstance(germeval2018.GermEval2018().get_label_name(), str)
+
