1a816c67c367d6ce609bcaf7eecab2c9ee0a5455
Akash Gautam <418akash@gmail.com>
2023-04-25 14:56:06 +0200

    new eval

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -39,7 +39,7 @@ def test_cls_evaluation():
 
 
     # new custom config for tiny model
-    custom_tiny_model_dir = os.getcwd() + "/tests/custom_model"
+    custom_tiny_model_dir = "./custom_model"
     print("custom_tiny_model_dir", custom_tiny_model_dir)
     #model_args = ModelArguments("/netscratch/agautam/experiments/finetune-evaluation-harness/tests/custom_model")
     #model_args.model_name_or_path = "/netscratch/agautam/experiments/finetune-evaluation-harness/tests/custom_model"
