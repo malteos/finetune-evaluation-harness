27139e5bdc6e22adb06ec1be3d1cfc247eb82e16
Akash Gautam <418akash@gmail.com>
2023-05-08 23:07:08 +0200

    changing version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name = "finetune_eval_harness",
-    version="0.4.0.dev0",
-    description="Finetune-Evaluation-Harness",
+    version="0.5.0.dev0",
+    description="Finetune_Eval_Harness",
     #long_description=open("README.md", "r", encoding="utf-8").read(),
     keywords="deep learning",
     author="DFKI Berlin",
