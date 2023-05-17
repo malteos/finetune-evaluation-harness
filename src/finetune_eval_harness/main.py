9c3a3267ea6a5e9fa96bd623454185f9b781f52f
Akash Gautam <418akash@gmail.com>
2023-05-18 00:41:41 +0200

    check

--- src/finetune_eval_harness/main.py
+++ src/finetune_eval_harness/main.py
@@ -1,8 +1,12 @@
 import sys
-import process_args
+sys.path.append('./')
+#import .process_args
+# .process_args import process_arguments
+from finetune_eval_harness.process_args import process_arguments
 
 def main():
-    process_args.process_arguments(sys.argv[1:])
+    #process_args.process_arguments(sys.argv[1:])
+    process_arguments(sys.argv[1:])
 
 if __name__ == "__main__":
     main()
